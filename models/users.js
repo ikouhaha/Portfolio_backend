2c706f01d78e10971a2a458bc565e72bfdfe3db1
Dennis Au Yeung <ikouhaha888@gmail.com>
2022-03-26 01:05:03 +0800

    d

--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
-const db = require('../helpers/database')
+const db = require('../helpers/mongodb')
 
+
+const collection = "articles"
 
 exports.findByUsername = async function (username) {
   const query = 'select * from users where username = ?'
@@ -22,18 +24,23 @@ exports.getById = async function (id) {
   return data
 }
 
+// exports.createUser = async function (user) {
+//   let keys = Object.keys(user)
+//   let values = Object.values(user)  
+//   keys = keys.join(',')   
+//   let parm = ''
+//   for(i=0; i<values.length; i++){ parm +='?,'}
+//   parm=parm.slice(0,-1)
+//   let query = `INSERT INTO users (${keys}) VALUES (${parm})`
+//   try{
+//     await db.run_query(query, values)  
+//     return {"status": 201}
+//   } catch(error) {
+//     return error
+//   }
+// }
+
 exports.createUser = async function (user) {
-  let keys = Object.keys(user)
-  let values = Object.values(user)  
-  keys = keys.join(',')   
-  let parm = ''
-  for(i=0; i<values.length; i++){ parm +='?,'}
-  parm=parm.slice(0,-1)
-  let query = `INSERT INTO users (${keys}) VALUES (${parm})`
-  try{
-    await db.run_query(query, values)  
-    return {"status": 201}
-  } catch(error) {
-    return error
-  }
+  let status = await db.run_insert(collection, user)
+  return status
 }
